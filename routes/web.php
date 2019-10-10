f901bfed8fafe721e56f5a5152c7248d1fbacd79
Bibi <beaves83@gmail.com>
2019-10-11 00:53:50 +0200

    cambios

--- routes/web.php
+++ routes/web.php
@@ -7,31 +7,28 @@ Route::get('/', function () {
 //Clientes
 Route::get('clientes', 'ClienteController@index')->name('clientes.index');//->middleware(ApiAuthMiddleware::class);
 Route::get('clientes/create', 'ClienteController@create')->name('clientes.create');
-Route::get('clientes/store', 'ClienteController@store');
-Route::get('clientes/all', 'ClienteController@all');
-Route::post('clientes/excel', 'ClienteController@toexcel')->name('clientes.excel');
-Route::get('clientes/{cliente}', 'ClienteController@show');
-Route::get('clientes/{cliente}/edit', 'ClienteController@edit');
-Route::get('clientes/update/{cliente}', 'ClienteController@update');
-Route::delete('clientes/{cliente}', 'ClienteController@destroy');
-
+Route::post('clientes', 'ClienteController@store')->name('clientes.store');
+Route::get('clientes/all', 'ClienteController@all')->name('clientes.all');
+Route::get('clientes/{cliente}', 'ClienteController@show')->name('clientes.show');
+Route::get('clientes/{cliente}/edit', 'ClienteController@edit')->name('clientes.edit');
+Route::put('clientes/{cliente}', 'ClienteController@update')->name('clientes.update');
+Route::delete('clientes/{cliente}', 'ClienteController@destroy')->name('clientes.destroy');
 
 //Citas
-Route::get('citas', 'CitaController@index');
-Route::get('citas/create', 'CitaController@create');
-Route::get('citas/store', 'CitaController@store');
-Route::get('citas/all', 'CitaController@all');
-Route::get('citas/{cita}', 'CitaController@show');
-Route::get('citas/{cita}/edit', 'CitaController@edit');
-Route::put('citas/{cita}', 'CitaController@update');
-Route::get('citas/confirmReserve', 'CitaController@confirmReserve');
-Route::delete('citas/{cita}', 'CitaController@destroy');
-Route::get('citas/update', 'ClienteController@update');
+Route::get('citas', 'CitaController@index')->name('citas.index');
+Route::get('citas/create', 'CitaController@create')->name('citas.create');
+Route::post('citas', 'CitaController@store')->name('citas.store');
+Route::get('citas/all', 'CitaController@all')->name('citas.all');
+Route::get('citas/{cita}', 'CitaController@show')->name('citas.show');
+Route::get('citas/{cita}/edit', 'CitaController@edit')->name('citas.edit');
+Route::put('citas/{cita}', 'CitaController@update')->name('citas.update');
+Route::get('citas/confirmReserve', 'CitaController@confirmReserve')->name('citas.confirmReserve');
+Route::delete('citas/{cita}', 'CitaController@destroy')->name('citas.destroy');
 
 //Usuario
-Route::get('usuarios', 'UserController@index');
-Route::get('usuarios/listado', 'UserController@all');
-Route::get('usuarios/register', 'UserController@register');
-Route::get('usuarios/update', 'UserController@update');
-Route::get('usuarios/store', 'UserController@store');
-Route::get('usuarios/destroy/{id}', 'UserController@destroy');
+Route::get('usuarios', 'UserController@index')->name('usuarios.index');
+Route::get('usuarios/listado', 'UserController@all')->name('usuarios.all');
+Route::get('usuarios/register', 'UserController@register')->name('usuarios.register');
+Route::put('usuarios/update', 'UserController@update')->name('usuarios.update');
+Route::post('usuarios/store', 'UserController@store')->name('usuarios.store');
+Route::delete('usuarios/destroy/{id}', 'UserController@destroy')->name('usuarios.destroy');
