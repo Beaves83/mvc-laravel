efd4367688d40a19a1df7e6a39375ff57a72a3a9
Bibiano Ruiz Crespo <brc61793@dominio.es>
2019-10-10 13:26:20 +0200

    Mejorando el backend

---
+++ public/js/datatable.js
@@ -0,0 +1,22 @@
+$(document).ready( function () {
+    $('#tablaclientes').DataTable({
+        dom: 'Bfrtip',
+        buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ]
+    } );
+    
+    $('#tablacitas').DataTable({
+        dom: 'Bfrtip',
+        buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ]
+    } );
+    
+    $('#tablavisitas').DataTable({
+        dom: 'Bfrtip',
+        buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ]
+    } );
+} )
